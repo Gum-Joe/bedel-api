ee4a9e468d30d4338ecfd34fa03d21c31415618e
Gum-Joe <kishansambhi@hotmail.co.uk>
2017-03-25 18:23:50 +0000

    :ambulance: Fix a typo
    :ambulance: Fix a typo causing logger prefix to be NaN

--- api/server/constants.js
+++ api/server/constants.js
@@ -13,7 +13,7 @@ var _chalk2 = _interopRequireDefault(_chalk);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /* Makes APi Logger prefix */
-var API_LOG_PREFIX = exports.API_LOG_PREFIX = -"API"; // Constants + utils
+var API_LOG_PREFIX = exports.API_LOG_PREFIX = "API"; // Constants + utils
 function MAKE_API_LOG_PREFIX(prefix) {
   return _chalk2.default.yellow(API_LOG_PREFIX + ":" + prefix);
 }
--- server/constants.js
+++ server/constants.js
@@ -2,7 +2,7 @@
 import chalk from "chalk";
 
 /* Makes APi Logger prefix */
-export const API_LOG_PREFIX =- "API";
+export const API_LOG_PREFIX = "API";
 export function MAKE_API_LOG_PREFIX(prefix) {
   return chalk.yellow(`${API_LOG_PREFIX}:${prefix}`);
 }
